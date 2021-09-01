4369d6224bbd72f365c6296cc61a886cd583e641
Gregory Bevan <gregory.bevan@zenika.com>
2021-09-01 10:19:20 +0200

    ✅ Enhance test for insert method - Add pool configuration for R2dbc connection

--- src/test/kotlin/me/elgregos/pocapi/PocapiApplicationTests.kt
+++ src/test/kotlin/me/elgregos/pocapi/PocapiApplicationTests.kt
@@ -3,6 +3,7 @@ package me.elgregos.pocapi
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.testcontainers.containers.PostgreSQLContainer
@@ -10,6 +11,7 @@ import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 
 @SpringBootTest
+@EnableR2dbcRepositories
 class PocapiApplicationTests {
 
     @Test
--- src/test/kotlin/me/elgregos/pocapi/repository/BookingRepositoryTest.kt
+++ src/test/kotlin/me/elgregos/pocapi/repository/BookingRepositoryTest.kt
@@ -6,23 +6,30 @@ import me.elgregos.pocapi.booking.infra.repository.insert
 import me.elgregos.pocapi.util.WithPostgresContainer
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.r2dbc.core.DatabaseClient
+import reactor.test.StepVerifier
+import java.time.Duration
 import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 @SpringBootTest
-class BookingRepositoryTest: WithPostgresContainer() {
+class BookingRepositoryTest : WithPostgresContainer() {
 
     @Autowired
     lateinit var bookingRepository: BookingRepository
+
+    @Autowired
+    lateinit var databaseClient: DatabaseClient
 
     @Test
     fun `should store new booking`() {
         val booking = Booking(UUID.randomUUID(), "This a test")
 
-        val result = bookingRepository.insert(booking).block()
-
-        assertEquals(booking, result)
-
+        bookingRepository.insert(booking).block(Duration.ofSeconds(5))
+        this.bookingRepository.findById(booking.id)
+            .`as` { StepVerifier.create(it) }
+            .expectNext(booking)
+            .verifyComplete()
     }
 }
